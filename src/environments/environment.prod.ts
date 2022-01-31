439afb949b18059381b18670586143143dd6ab2a
ahmed shaikh <ahmedshaikh78699@gmail.com>
2022-01-31 10:36:24 +0530

    production environment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-   //ApiUrl: 'https://bandhanapi.prismcement.com',
-   ApiUrl: 'http://172.20.1.124:8003',
+   ApiUrl: 'https://bandhanapi.prismcement.com',
+  // ApiUrl: 'http://172.20.1.124:8003',
   //  ApiUrl: 'http://103.131.93.13:83',
   //  ApiUrl: 'http://192.168.1.101:5000',
 };
