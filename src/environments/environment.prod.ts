b80f97d77bd53fc36bf2af4e1abe62f2984b036a
ahmed shaikh <ahmedshaikh78699@gmail.com>
2022-06-11 09:57:45 +0530

    Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-   ApiUrl: 'https://bandhan.prismcement.com',
+   ApiUrl: 'https://bandhan1.prismcement.com',
    //ApiUrl: 'http://172.20.1.124:44335',
- // ApiUrl: 'https://localhost:44335',
+  //ApiUrl: 'https://localhost:44335',
   // ApiUrl: 'http://172.20.1.124:8003',
   //  ApiUrl: 'http://103.131.93.13:83',
   //  ApiUrl: 'http://192.168.1.101:5000',
